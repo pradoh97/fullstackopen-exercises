3a73c88d2322de91d1a29e3bc76682653f996b2d
Hernán <pradoh97@gmail.com>
2023-12-25 13:45:51 -0300

    Add lesson 1 practices

---
+++ lessons-excercises/1part/part1/src/App.jsx
@@ -0,0 +1,27 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <p>Hello {props.name}, you are {props.age} years old.</p>
+  )
+}
+const App = () => {
+  const friends = ['Pietro', 'Peter']
+  const person = [
+    { name: "Pietro", age: 4},
+    { name: "Peter", age: 10}
+  ]
+  const name = 'Peter'
+  const age = 10
+  return (
+    <>
+      <p>Greetings</p>
+      <Hello name={name} age={age} />
+      <Hello name='Daisy' age={20} />
+      <Hello name='Test' age={30+10} />
+      <p>{person[0].name}</p>
+      <p>{friends}</p>
+    </>
+  )
+}
+
+export default App
