a52edcb6c2fe76008b2acc4d035d23b4b2fa146a
Hernán <pradoh97@gmail.com>
2024-02-02 23:01:24 -0300

    Add controlled components

--- lessons-exercises/part2/rendering-collections/src/App.jsx
+++ lessons-exercises/part2/rendering-collections/src/App.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({notes}) => {
+const App = props => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  
+  const handleNoteChange = event => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -10,6 +30,10 @@ const App = ({notes}) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
