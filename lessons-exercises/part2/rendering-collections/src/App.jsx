77f9dc792b1b5fdb67b7154e94118e3b163592d2
Hernán <pradoh97@gmail.com>
2024-03-07 13:41:44 -0300

    Fetch data from server

--- lessons-exercises/part2/rendering-collections/src/App.jsx
+++ lessons-exercises/part2/rendering-collections/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = props => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://dev-hernan-test.duckdns.org:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
+
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   const handleNoteChange = event => setNewNote(event.target.value)
