75e7a694752f2269e597f938779d402f24f315ee
Hernán <pradoh97@gmail.com>
2024-03-11 19:41:21 -0300

    Add note creation

--- lessons-exercises/part2/rendering-collections/src/App.jsx
+++ lessons-exercises/part2/rendering-collections/src/App.jsx
@@ -1,40 +1,48 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
-
+import noteService from '.services/notes'
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   
   const hook = () => {
-    console.log('effect')
     axios
       .get('http://dev-hernan-test.duckdns.org:3001/notes')
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
   }
 
   useEffect(hook, [])
 
-  console.log('render', notes.length, 'notes')
-
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   const handleNoteChange = event => setNewNote(event.target.value)
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://dev-hernan-test.duckdns.org:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    axios.put(url, changedNote).then(response => { setNotes(notes.map(n => n.id !== id ? n : response.data))})
+  }
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://dev-hernan-test.duckdns.org:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+
   }
 
   return (
@@ -45,7 +53,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit={addNote}>
--- lessons-exercises/part2/rendering-collections/src/components/Note.jsx
+++ lessons-exercises/part2/rendering-collections/src/components/Note.jsx
@@ -1,7 +1,9 @@
-const Note = ({note}) => {
+const Note = ({note, toggleImportance}) => {
+    const label = note.important ? 'mark as unimportant' : 'mark as important'
+    
     return (
-    <li>{note.content}</li>
- )
+        <li>{note.content} <button onClick={toggleImportance}>{label}</button></li>
+    )
 }
 
 export default Note
