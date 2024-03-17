8fbc0a82f9229a9d723059a249458f717dfa60cb
Hernán <pradoh97@gmail.com>
2024-03-17 12:43:00 -0300

    Add toggle show info for country on list of countries

--- countries/src/components/SearchResults.jsx
+++ countries/src/components/SearchResults.jsx
@@ -1,11 +1,17 @@
-const SearchResults = ({countryList}) => {
+import countriesService from '../services/countries'
 
-    if(countryList.length > 10) return(<p>Too many matches, try adding more letters for the country name.</p>)
+const toggleCountryInfo = (country) => {
+    const countryElement = document.getElementById(country.name.common).querySelectorAll('div')[0]
+    countryElement.style.display === "none" ? countryElement.style.display = "initial" : countryElement.style.display = "none"
+}
 
-    if(countryList.length == 1) {
-        const country = countryList[0]
-        return (
-            <>
+const SearchResult = ({hidden, country}) => {
+    const hiddenStyle = {
+        display: "none"
+    }
+    return(
+        
+        <div style={hidden ? hiddenStyle : ""}>
             <h1>{country.name.common}</h1>
             <p>Capital: {country.capital[0]}</p>
             <p>Area: {country.area}</p>
@@ -15,12 +21,29 @@ const SearchResults = ({countryList}) => {
                 {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
             </ul>
             <img src={country.flags.png} alt={country.flag} />
-            </>
-        )
-    }
-    return(
-        countryList.map(country => <p key={country.name.common}>{country.name.common}</p>)
+        </div>
     )
+}
+const SearchResults = ({countryList}) => {
+
+    if(countryList.length > 10) return(<p>Too many matches, try adding more letters for the country name.</p>)
+
+    if(countryList.length == 1) {
+        const country = countryList[0]
+        return (<SearchResult country={country} />)
+    }
+    
+    return(
+        countryList.map(country => {
+        return (
+            <div key={country.name.common} id={country.name.common}>
+            <p key={country.name.common + " - p"}>{country.name.common}</p>
+            <button key={country.name.common + " - toggle button"} onClick={() => toggleCountryInfo(country)}>Show</button>
+            <SearchResult hidden={true} key={country.name.common + " - toggle"} country={country} />
+            </div>
+        )
+        })  
+    )  
 }
 
 export default SearchResults
--- countries/src/services/countries.js
+++ countries/src/services/countries.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const getCountries = () => axios.get(`${urlAllCountries}`)
+const getCountry = name => axios.get(`${urlCountry}/${name}`)
 
 const url = "https://studies.cs.helsinki.fi/restcountries/api"
 const urlAllCountries = url + "/all"
-export default {getCountries}
+const urlCountry = url + "/name"
+export default {getCountries, getCountry}
